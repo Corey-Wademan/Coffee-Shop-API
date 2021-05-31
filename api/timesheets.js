e355231596a3509a4eeb89d682cc953ff6fd160d
Corey Wademan <67563215+Corey-Wademan@users.noreply.github.com>
2021-05-31 12:42:25 -0400

    Added ROUTES/QUERY for GET/POST Timesheets

---
+++ api/timesheets.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const timesheetsRouter = express.Router({ mergeParams: true });
+const sqlite = require('sqlite3');
+const db = new sqlite.Database(process.env.TEST_DATABASE || './database.sqlite');
+
+timesheetsRouter.param('timesheetId', (req, res, next, timeSheetId) => {
+    db.get(`SELECT * FROM Timesheet WHERE Timesheet.id = $timeSheetId`, {$timeSheetId: timeSheetId}, (err, timesheet) => {
+        if (err) {
+            next(err);
+        } else if (timesheet) {
+            next();
+        } else {
+            return res.sendStatus(404)
+        }
+    });
+})
+
+timesheetsRouter.get('/', (req, res, next) => {
+    db.all(`SELECT * FROM Timesheet WHERE Timesheet.employee_id = ${req.params.employeeId}`, (err, timesheets) => {
+        if (err) {
+            next(err); 
+        } else {
+            res.status(200).json({timesheets:timesheets})
+        }
+    });
+});
+
+timesheetsRouter.post('/', (req, res, next) => {
+    const hours = req.body.timesheet.hours,
+        rate = req.body.timesheet.rate,
+        date = req.body.timesheet.date,
+        employeeId = req.params.employeeId;
+    if (!hours || !rate || !date) {
+        return res.sendStatus(400);
+    }
+
+    db.run(`INSERT INTO Timesheet (hours, rate, date, employee_id)
+            VALUES ($hours, $rate, $date, $employeeId)
+    `, {
+        $hours: hours,
+        $rate: rate,
+        $date: date,
+        $employeeId: employeeId
+    }, function (error) {
+        if (error) {
+            next(error);
+        } else {
+            db.get(`SELECT * FROM Timesheet WHERE Timesheet.id = ${this.lastID}`, (err, timesheet) => {
+                res.status(201).json({ timesheet: timesheet })
+            });
+        }
+    });
+});
+
+module.exports = timesheetsRouter;
