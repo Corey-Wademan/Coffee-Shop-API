75d9eadb3e32cb2b8031a5bcc861a34e5239c6c0
Corey Wademan <67563215+Corey-Wademan@users.noreply.github.com>
2021-06-01 00:49:27 -0400

    Added /:menuItemId PARAMS

--- api/menuItem.js
+++ api/menuItem.js
@@ -3,6 +3,18 @@ const menuItemsRouter = express.Router({ mergeParams:true });
 const sqlite = require('sqlite3');
 const db = new sqlite.Database(process.env.TEST_DATABASE || './database.sqlite');
 
+menuItemsRouter.param('menuItemId', (req, res, next, menuItemId) => {
+    db.get(`SELECT * FROM MenuItem WHERE MenuItem.id = $menuItemId`, { menuItemId: menuItemId }, (err, menuItem) => {
+        if (err) {
+            next(err)
+        } else if (menuItem) {
+            next();
+        } else {
+            res.sendStatus(404)
+        }
+    })
+});
+
 menuItemsRouter.get('/', (req, res, next) => {
     db.all(`SELECT * FROM MenuItem WHERE MenuItem.menu_id = ${req.params.menuId}`, (err, menuItems) => {
         if (err) {
